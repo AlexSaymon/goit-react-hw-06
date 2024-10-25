278daa0bd94483c58cce58453536bf7394ba8f5e
Oleksandr Kurchenko <151569683+AlexSaymon@users.noreply.github.com>
2024-10-25 12:00:14 -0500

    commit

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,14 +1,17 @@
-import { useDispatch } from "react-redux";
-import { changeFilter } from "../../redux/filtersSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
 
 const SearchBox = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(selectNameFilter);
 
   const handleChange = (event) => {
     dispatch(changeFilter(event.target.value));
   };
 
-  return <input type="text" placeholder="Search" onChange={handleChange} />;
+  return (
+    <input type="text" placeholder="Search" value={filter} onChange={handleChange} />
+  );
 };
 
 export default SearchBox;
